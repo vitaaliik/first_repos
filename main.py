68213dad8226e2adaae1b1b240ad55914849e338
vitalik <vitaliklutchak12@gmail.com>
2024-04-18 20:47:04 +0300

    new DZ

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print("Hello World!")
+from datetime import datetime
 
-print("Hello Git")
+def get_days_from_today(date):
+    
+    try:
+        day = datetime.strptime(date, '%Y-%m-%d').date()
+    except ValueError:
+        return "Неправильний формат дати. Використовуйте формат 'РРРР-ММ-ДД'."
+
+    day_today = datetime.today().date()
+    difference = (day_today - day).days
+    return difference
+
+print(get_days_from_today('2007-03-12'))
