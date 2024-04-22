97a349ed2aa590b32c52661ab84e6febf824c0f0
vitalik <vitaliklutchak12@gmail.com>
2024-04-22 20:12:46 +0300

    new dz2

--- dz2.py
+++ dz2.py
@@ -1,15 +1,17 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if not (1 <= min <= max <= 1000):
+    if not (1 <= min <= max <= 1000) or quantity > (max - min + 1):
+        print("Некоректні дані. Будь ласка, перевірте вхідні параметри.")
         return []
     
-    numbers= set()
+    numbers = set()
     while len(numbers) < quantity:
         numbers.add(random.randint(min, max))
 
     sorted_numbers = sorted(numbers)
     return sorted_numbers
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
-print("Ваші лотерейні числа:", lottery_numbers)
+lottery_numbers = get_numbers_ticket(4, 50, 6)
+if lottery_numbers:
+    print("Ваші лотерейні числа:", lottery_numbers)
